ae486c9c25fd8b581b6650476600a22778e861e1
GS26Surya <gsurya124@gmail.com>
2023-11-03 17:01:05 +0530

    Intial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Main from './components/main'
+import WorkSpace from './components/workspace'
+
+function App() {
+
+  return (
+   <div className="app ">
+    <WorkSpace>
+      <Main/>
+    </WorkSpace>
+   </div>
+  )
+}
+
+export default App
